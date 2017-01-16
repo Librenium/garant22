66f665594af78c8cdf0e3b9c9d8e0bb0016ffff5
Librenium <exenced@gmail.com>
2017-01-16 20:41:03 +0400

    ;

---
+++ common.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+	$("#callback, .forms, .fancybox").fancybox({
+			padding : 0
+	});
+
+	$(".fancy-group").fancybox({
+		'transitionIn'	:	'elastic',
+		'transitionOut'	:	'elastic',
+		'speedIn'		:	600, 
+		'speedOut'		:	200, 
+		'overlayShow'	:	false
+	});
+
+	$(".about-item div").height('auto').equalHeights();
+
+
+	$("#form").submit(function() {
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
+			$("#form").trigger("reset");
+		});
+		return false;
+	});
+});
